98c36175d6e762c214e4fb290c7917c2467d24f7
Nimsara Chamindu <nimsarachamidhu@gmail.com>
2025-10-29 10:25:45 +0530

    Implement basic functionality of Tenzies project

--- Tenzies/Tenzies.jsx
+++ Tenzies/Tenzies.jsx
@@ -1,22 +1,104 @@
+import { useEffect, useState, useRef } from "react";
+
 import Die from "./components/Die";
 import "./Tenzies.css";
 
 export default function Tenzies(props) {
+  const [diceInfoList, setDiceInfoList] = useState([]);
+  const choosenVal = useRef(null);
+
+  useEffect(() => {
+    if (diceInfoList.length <= 0) {
+      reset();
+    }
+  }, []);
+
+  function isWon() {
+    let isWon = true;
+    diceInfoList.forEach((die) =>
+      die.state === 0 || die.state === 2 ? (isWon = false) : null
+    );
+    return isWon;
+  }
+
+  function reset() {
+    let _diceInfoList = [];
+    choosenVal.current = null;
+
+    generateRandomVals(10).forEach((randVal, index) => {
+      _diceInfoList.push({
+        id: index,
+        value: randVal,
+        state: 0,
+      });
+    });
+    setDiceInfoList(_diceInfoList);
+  }
+
+  function calculateState(dieVal) {
+    console.log(
+      "CalculateState() => choosenVal: " +
+        choosenVal.current +
+        "\tdieVal: " +
+        dieVal
+    );
+
+    if (choosenVal.current !== null && choosenVal.current === dieVal) {
+      return 1;
+    } else if (choosenVal.current !== null && choosenVal.current !== dieVal) {
+      return 2;
+    } else {
+      choosenVal.current = dieVal;
+      return 1;
+    }
+  }
+
+  function onDieClick(id) {
+    console.log("Die id: " + id);
+
+    const newValList = generateRandomVals(10);
+
+    setDiceInfoList((prevDiceInfoList) => {
+      const updatedDiceInfo = prevDiceInfoList.map((dieInfo, index) =>
+        dieInfo.id === id
+          ? { ...dieInfo, state: calculateState(dieInfo.value) }
+          : dieInfo.state === 1 || dieInfo.state === 2
+          ? dieInfo
+          : { ...dieInfo, value: newValList[index] }
+      );
+      return updatedDiceInfo;
+    });
+
+    setTimeout(() => {
+      if (isWon()) console.log("You Won!!");
+    }, 300);
+
+    console.log("Updated choosenVal: " + choosenVal.current);
+    //diceInfoList[id].state = 1;
+  }
+
   return (
     <div className="tenzies-page">
       <h1>Tenzies Game</h1>
       <div className="tenzies-dice-container">
-        <Die value={2} />
-        <Die value={3} />
-        <Die value={4} />
-        <Die value={5} />
-        <Die value={6} />
-        <Die value={7} />
-        <Die value={8} />
-        <Die value={9} />
-        <Die value={1} />
-        <Die value={1} />
+        {diceInfoList.map((dieInfo, index) => (
+          <Die key={dieInfo.id} info={dieInfo} onclick={onDieClick} />
+        ))}
+      </div>
+      <div className="tenzies-restart-btn-container">
+        <button onClick={reset} className="tenzies-restart-btn">
+          Restart
+        </button>
       </div>
     </div>
   );
+}
+
+function generateRandomVals(count = 10) {
+  let numList = [];
+  for (let i = 0; i < count; i++) {
+    numList.push(Math.ceil(Math.random() * 9));
+  }
+  console.log(numList);
+  return numList;
 }
--- Tenzies/components/Die.jsx
+++ Tenzies/components/Die.jsx
@@ -1,3 +1,14 @@
 export default function Die(props) {
-  return <button className="tenzies-die">{props.value}</button>;
+  console.log("Die rendered!");
+  return (
+    <button
+      id={props.info.id}
+      className={`tenzies-die ${props.info.state === 1 ? "correct" : null} ${
+        props.info.state === 2 ? "false" : null
+      }`}
+      onClick={() => props.onclick(props.info.id)}
+    >
+      {props.info.value}
+    </button>
+  );
 }
